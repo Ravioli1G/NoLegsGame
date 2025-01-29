df582bbb93ac83f1549df628803cbb6c6daa97e1
Ravioli1G <114981973+Ravioli1G@users.noreply.github.com>
2025-01-29 17:39:06 +1100

    Added basic enemy implementation

--- Assets/Scripts/Enemy/EnemyBrainRworded.cs
+++ Assets/Scripts/Enemy/EnemyBrainRworded.cs
@@ -7,6 +7,7 @@ public class EnemyBrainRworded : MonoBehaviour
     private EnemyReferences enemyReferences;
 
     private float shootingDistance;
+    private float pathUpdateDelay;
 
     private void Awake()
     {
@@ -20,6 +21,37 @@ public class EnemyBrainRworded : MonoBehaviour
 
     void Update()
     {
-        
+        if (target != null)
+        {
+            bool inRange = Vector3.Distance(transform.position, target.position) <= shootingDistance;
+
+            if (inRange)
+            {
+                LookAtTarget();
+            }
+            else
+            {
+                UpdatePath();
+            }
+        }
+
     }
+
+    void LookAtTarget() 
+    { 
+        Vector3 lookPos = target.position - transform.position;
+        lookPos.y = 0f;
+        Quaternion rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.2f);
+    }
+
+    void UpdatePath() 
+    {
+        if (Time.time >= pathUpdateDelay)
+        {
+            pathUpdateDelay = Time.time + enemyReferences.pathUpdateDelay;
+            enemyReferences.agent.SetDestination(target.position);
+        }
+    }
+        
 }
--- Assets/Scripts/Enemy/EnemyReferences.cs
+++ Assets/Scripts/Enemy/EnemyReferences.cs
@@ -6,6 +6,9 @@ public class EnemyReferences : MonoBehaviour
     public NavMeshAgent agent;
     public Animator animator;
 
+    [Header("stats")]
+    public float pathUpdateDelay = 0.2f;
+
     private void Awake()
     {
         animator = GetComponent<Animator>();
